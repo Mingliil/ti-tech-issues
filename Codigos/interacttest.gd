8f479c48c7680dd80915565717333d4c2e706b4f
Mingliil <miglegames.lmsnts@gmail.com>
2026-03-23 23:02:41 -0300

    aaaaaaaaaaaa

--- Codigos/interacttest.gd
+++ Codigos/interacttest.gd
@@ -1,26 +1,32 @@
 extends Node3D
-
+var Player
 var TextoInte
+var IntArea
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	TextoInte = get_node("Label3D")
-	
-	pass # Replace with function body.
+	IntArea = get_node("Area de interação")
+	IntArea.area_entered.connect(_on_area_3d_area_entered)
+	IntArea.area_exited.connect(_on_area_3d_area_exited)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
 	pass
 
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	TextoInte.text = "aperte E para interagir"
+	print("foi")
 	add_to_group("Player")
+	Player = get_tree().get_first_node_in_group("Player")
+	print(Player)
+	if not Player.interact.connect(_on_player_interact):
+		Player.interact.connect(_on_player_interact)
 
 func _on_area_3d_area_exited(area: Area3D) -> void:
 	remove_from_group("Player")
 	TextoInte.text = ""
-	
+
 func _on_player_interact(Interact: Variant) -> void:
 	if is_in_group("Player"):
 		print("foi")
@@ -28,5 +34,7 @@ func _on_player_interact(Interact: Variant) -> void:
 			var afonso = get_node("AFONSO")
 			if afonso.visible == false:
 				afonso.visible = true
+				var chat = ResourceLoader.load("res://GUIandHUDS/chat_gui.tscn")
+				add_child(chat)
 			else:
 				afonso.visible = false
