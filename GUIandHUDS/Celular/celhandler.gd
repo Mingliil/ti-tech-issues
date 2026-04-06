2aa0ff8b5dc6bdc809b5529be4a08c84512938a0
Mingliil <miglegames.lmsnts@gmail.com>
2026-04-06 13:18:15 -0300

    setup cell1

--- GUIandHUDS/Celular/celhandler.gd
+++ GUIandHUDS/Celular/celhandler.gd
@@ -1,7 +1,8 @@
 extends Control
 
 @onready var player
-@onready var textoTest = $tela/RichTextLabel
+@onready var appUso: ReferenceRect = $tela/appUso
+@onready var textoTest: RichTextLabel = $tela/RichTextLabel
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("Player")
@@ -11,5 +12,5 @@ func _process(delta: float) -> void:
 	var miHdr = preload("res://Codigos/MissionHandler.gd").new()
 	if Input.is_action_just_pressed("ui_up"):
 		for i in player.PlayerStats[1].missao.size():
-			textoTest.text = miHdr._get_missions(get_tree().get_first_node_in_group("Player"), i)
+			textoTest.text += miHdr._get_missions(get_tree().get_first_node_in_group("Player"), i)
 	pass
