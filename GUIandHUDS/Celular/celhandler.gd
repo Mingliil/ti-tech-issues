ffaa7335915151bc8ba974948a0aef05ca145c75
Mingliil <miglegames.lmsnts@gmail.com>
2026-04-06 13:35:15 -0300

    Update celhandler.gd

--- GUIandHUDS/Celular/celhandler.gd
+++ GUIandHUDS/Celular/celhandler.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var player
+@onready var botaoMissao = preload("res://GUIandHUDS/Celular/botaomissao.tscn")
 @onready var appUso: ReferenceRect = $tela/appUso
 @onready var textoTest: RichTextLabel = $tela/RichTextLabel
 # Called when the node enters the scene tree for the first time.
@@ -12,5 +13,7 @@ func _process(delta: float) -> void:
 	var miHdr = preload("res://Codigos/MissionHandler.gd").new()
 	if Input.is_action_just_pressed("ui_up"):
 		for i in player.PlayerStats[1].missao.size():
-			textoTest.text += miHdr._get_missions(get_tree().get_first_node_in_group("Player"), i)
+			var btMissao = botaoMissao.instantiate()
+			btMissao.get_node("Button").text += miHdr._get_missions(get_tree().get_first_node_in_group("Player"), i)
+			appUso.add_child(btMissao)
 	pass
