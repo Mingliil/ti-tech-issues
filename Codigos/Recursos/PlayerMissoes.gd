eb1172f9ba451ff8311996c3e4b9f4d7bca34f2d
Mingliil <miglegames.lmsnts@gmail.com>
2026-04-16 13:28:25 -0300

    https://c.tenor.com/S_uqsAIR1HoAAAAd/tenor.gif
    https://c.tenor.com/S_uqsAIR1HoAAAAd/tenor.gif

--- Codigos/Recursos/PlayerMissoes.gd
+++ Codigos/Recursos/PlayerMissoes.gd
@@ -6,10 +6,10 @@ class_name MissoesData
 @export var missaoDada: bool
 #parte base, fala o nome/titulo e então uma descrição
 @export var nomeMissao: String
-@export_multiline() var descricao: String
+@export_multiline var descricao: String
 #requisitos para concluir a missão
 @export var missoesRequisitosNome: Array[String]
 @export var missoesRequisitosInfo: Array[MissaoRequis]
 
-@export var recompensaTexto: String
+@export_multiline var recompensaTexto: String
 @export var recompensaVar: Array
--- GUIandHUDS/Celular/Aplicativos/missionInf.gd
+++ GUIandHUDS/Celular/Aplicativos/missionInf.gd
@@ -2,18 +2,29 @@ extends Control
 
 @export var MissionInf: MissoesData
 @onready var preloadRecBotao = preload("res://GUIandHUDS/Celular/Aplicativos/MissioHub/requisito_missao.tscn")
-@onready var Vbox: VBoxContainer = $AreaApp/AreaNucleo/VBoxContainer
+@onready var Vbox: VBoxContainer = $AreaApp/VBoxContainer
 func _ready() -> void:
 	Vbox.get_node("MissaoTit").text = MissionInf.nomeMissao
 	Vbox.get_node("MissaoDesc").text = MissionInf.descricao
 	Vbox.get_node("MissaoRec").text = MissionInf.recompensaTexto
 	for i in MissionInf.missoesRequisitosNome.size():
 		_missao_requis(i)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
 func _missao_requis(i: int) -> void:
 	var recBotao = preloadRecBotao.instantiate()
-	recBotao.text = MissionInf.missoesRequisitosNome[i]
-	get_node("AreaApp/AreaNucleo/VBoxContainer/Requisitos").add_child(recBotao)
+	if MissionInf.missoesRequisitosInfo[i].RequisitoMax is not bool:
+		recBotao.text = MissionInf.missoesRequisitosNome[i] + "(%x/%s)" %[MissionInf.missoesRequisitosInfo[i].RequisitoAtual,MissionInf.missoesRequisitosInfo[i].RequisitoMax]
+	else:
+		var recAtual = 0
+		var recMax = 0
+		if MissionInf.missoesRequisitosInfo[i].RequisitoAtual:
+			recAtual = 1
+		if MissionInf.missoesRequisitosInfo[i].RequisitoMax:
+			recMax = 1
+		recBotao.text = MissionInf.missoesRequisitosNome[i] + "(%d/%s)" %[recAtual,recMax]
+	Vbox.get_node("Requisitos").add_child(recBotao)
