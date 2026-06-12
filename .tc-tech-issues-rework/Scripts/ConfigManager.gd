af7908570b8fe5f1a81c7cac34b292bf54bd8a93
Mingliil <miglegames.lmsnts@gmail.com>
2026-06-11 21:38:03 -0300

    gallop on, Rocinante!;

--- .tc-tech-issues-rework/Scripts/ConfigManager.gd
+++ .tc-tech-issues-rework/Scripts/ConfigManager.gd
@@ -2,13 +2,13 @@ extends Node
 
 var root
 var cfg = ConfigFile.new()
-var err = cfg.load("res://Cenas/MainMenu/Scripts/Recursos/config.cfg")
+var err = cfg.load("res://Scripts/Recursos/config.cfg")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if err != OK:
 		print("erro carregando config" + str(err))
 	else:
 		print("config normal")
-	
-	root = get_tree().root
-	
+	var e = cfg.get_value("grafico", "res")
+	print(e)
+	get_viewport().size = e
