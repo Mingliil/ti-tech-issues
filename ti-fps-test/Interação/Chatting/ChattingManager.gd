7d56ed223ad137d2d89842b3c4c4a4f66799a71d
Mingliil <miglegames.lmsnts@gmail.com>
2026-06-30 15:43:58 -0300

    IM A FRIKING ARCHITECT

--- ti-fps-test/Interação/Chatting/ChattingManager.gd
+++ ti-fps-test/Interação/Chatting/ChattingManager.gd
@@ -8,7 +8,11 @@ extends Node
 var PlayerRoot
 var player
 var ItemAtual: int = 0
-@export var FalaData: SpeakerData
+@onready var ImgFala = $TextureRect
+@onready var nomefalante: Label = $VBoxContainer/NomeFalante
+@onready var falaTexto: RichTextLabel = $VBoxContainer/PanelContainer/MarginContainer/FalaTexto
+@export var DialogueData: diagData
+
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("PLAYER").get_child(0)
 	player.interagindo = true
@@ -16,12 +20,16 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
 func _process(delta: float) -> void:
-	if ItemAtual >  FalaData.falas.size():
+	if ItemAtual+1 >  DialogueData.Conversas[0].falas.size():
 		player.interagindo = false
-		queue_free()
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	
-	
+		queue_free()
+	else:
+		if DialogueData.Conversas[0].falas[ItemAtual].FalanteImg:
+			ImgFala.texture =DialogueData.Conversas[0].falas[ItemAtual].FalanteImg
+			#ImgFala.offset.y = DialogueData.Conversas[0].falas[ItemAtual].FalanteImg.get_size().y
+		falaTexto.text = DialogueData.Conversas[0].falas[ItemAtual].texto
+		nomefalante.text =DialogueData.Conversas[0].falas[ItemAtual].FalanteNome
 	if Input.is_action_just_pressed("interagir"):
 		ItemAtual +=1
 	pass
---
+++ ti-fps-test/Interação/Chatting/diagData.gd
@@ -0,0 +1,8 @@
+extends UniResExpo
+class_name diagData
+
+
+@export_range(0, 99, 1) var diagRandomStart: float = 0
+@export_range(0, 99, 1) var diagRandomEnd: float = 0
+@export var primeiroDialogo: int = 0
+@export var Conversas: Array[SpeakerData]
