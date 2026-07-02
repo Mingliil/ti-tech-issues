bbd42cbb0ac8565e05ed9616c81ccc4bf99199c3
Mingliil <miglegames.lmsnts@gmail.com>
2026-07-02 13:18:40 -0300

    BAMBOO HATTED KIM IS THE GOAT;

--- ti-fps-test/Interação/Chatting/ChattingManager.gd
+++ ti-fps-test/Interação/Chatting/ChattingManager.gd
@@ -11,9 +11,10 @@ var ItemAtual: int = 0
 @onready var ImgFala = $TextureRect
 @onready var nomefalante: Label = $VBoxContainer/NomeFalante
 @onready var falaTexto: RichTextLabel = $VBoxContainer/PanelContainer/MarginContainer/FalaTexto
+@onready var buttonBox = $VBoxContainer/PanelContainer/ButtonBox
 @export var DialogueData: diagData
-
 @onready var OptButtonPreload = preload("res://Interação/Chatting/option_button.tscn")
+var conversa: int = 0
 var proxima_Etapa = true
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("PLAYER").get_child(0)
@@ -22,32 +23,53 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
 func _process(delta: float) -> void:
-	if ItemAtual >=  DialogueData.Conversas[0].falas.size():
+	
+	if ItemAtual >=  DialogueData.Conversas[conversa].falas.size():
 		player.interagindo = false
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		queue_free()
-	if proxima_Etapa:
+	
+	elif proxima_Etapa:
 		print(ItemAtual)
 		proxima_Etapa = false
 		var item
 		if DialogueData.DialogoRng:
 			item = DialogueData.Conversas[randi_range(DialogueData.diagRandomStart,DialogueData.diagRandomEnd)]
 			print(item)
-		else:
+		elif conversa != 0:
 			item = DialogueData.Conversas[DialogueData.primeiroDialogo]
+		else:
+			item = DialogueData.Conversas[conversa]
 		if item.falas[ItemAtual] is TextData:
 			Dialogo()
 		elif item.falas[ItemAtual] is EscolhaDiagSize:
-			Escolha()
-	
+			Escolha(item.falas[ItemAtual])
 	pass
 
 func Funcao() -> void:
 	pass
-func Escolha() -> void:
-	pass
+
+func Escolha(data) -> void:
+	var buttonInst = OptButtonPreload.instantiate()
+	falaTexto.get_parent().visible = false
+	for i in data.Opcoes.size():
+		buttonInst.text = data.Opcoes[i].ButtaoTexto
+		buttonInst.connect("pressed",Callable(self, "botao_apertado").bind(data.Opcoes[i].DialogoSeguinte),CONNECT_ONE_SHOT)
+		buttonBox.add_child(buttonInst)
+
+func botao_apertado(next_diag) -> void:
+	for i in buttonBox.get_children():
+		i.queue_free()
+	if next_diag != 0:
+		conversa = next_diag
+		ItemAtual = 0
+	else:
+		ItemAtual += 1
+	proxima_Etapa = true
+	print(conversa)
+
 func Dialogo() -> void:
-	
+	falaTexto.get_parent().visible = true
 	if DialogueData.Conversas[0].falas[ItemAtual].FalanteImg:
 		ImgFala.texture =DialogueData.Conversas[0].falas[ItemAtual].FalanteImg
 		#ImgFala.offset.y = DialogueData.Conversas[0].falas[ItemAtual].FalanteImg.get_size().y
