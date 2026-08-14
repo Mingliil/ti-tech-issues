e132a94aa70ecfab344e3df83c4aace5ac87b888
Mingliil <miglegames.lmsnts@gmail.com>
2026-08-13 22:30:44 -0300

    ASDADASDWADSAW

--- ti--technical-issues-for-hire!/Objetos/portaBehavior.gd
+++ ti--technical-issues-for-hire!/Objetos/portaBehavior.gd
@@ -4,19 +4,18 @@ extends Node3D
 var fechado: bool = false
 @export var podetrancar: bool = false
 @onready var corpo = $Hinge/PortaCorpo
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
 	if corpo.rotation_degrees.x <= -89.9 and podetrancar:
 		fechado = true
-		podetrancar = false
 	if !podetrancar:
 		fechado = false
+
 	if fechado:
 		corpo.lock_rotation = true
 	else:
 		corpo.lock_rotation = false
 
-func _on_timeout() -> void:
-	podetrancar = true
+func _entered(body: Node3D) -> void:
+	if body == get_node("Hinge/PortaCorpo"):
+		podetrancar = true
